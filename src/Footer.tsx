699980fa7242eef5ccd416c679c2c776b3eb8cb9
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-06-08 14:01:14 +0530

    Adding Code

---
+++ src/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>
+        <p>&copy; Copyright By <a target="_blank" href="#">ShivamGupta</a>. Intended for learning or my portfolio</p>
+        <p><span className="u-bold u-italic">Not allowed</span> to use as your own portfolio.</p>
+      </small>
+    </footer>
+  );
+}
---
+++ src/HashtagList.tsx
@@ -0,0 +1,5 @@
+export default function HashtagList() {
+  return (
+    <ul>HashtagList</ul>
+  )
+}
